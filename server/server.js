14a3f396dc2d38118261a00560a32555d5197ab3
Collin F <thedrunkninja98@gmail.com>
2025-10-20 15:05:50 -0400

    More files to move around

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const path = require("path");
-const User = require("./models/User");
+const User = require("./models/userModel.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
