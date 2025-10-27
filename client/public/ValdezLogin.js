5c6a961dc6c4b1c6f5db1897b0f3ef5f2fcd62fe
Collin F <thedrunkninja98@gmail.com>
2025-10-27 15:41:04 -0400

    auth is now users
    As per project desc

--- client/public/ValdezLogin.js
+++ client/public/ValdezLogin.js
@@ -10,7 +10,7 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
     }
 
     try {
-        const response = await fetch("/api/auth/login", {
+        const response = await fetch("/api/users/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password })
--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // Serve static files (HTML, CSS, JS, images)
 app.use(express.static(path.join(__dirname, "../client/public")));
 
-app.use("/api/auth", authRoutes);
+app.use("/api/users", authRoutes);
 
 mongoose.connect(MONGO_URI)
     .then(() => console.log("Connected to DB"))
