8e9d4352121a872e1027a6ed01ae53235578009c
PDiabelko <paigediabelko@gmail.com>
2025-11-10 17:41:50 -0500

    Update userRoutes.js
    route changes for changing password

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,3 +1,6 @@
+import express from "express";
+import { changePassword } from "../controllers/userController.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
 const express = require("express");
 const { createUser, getAllUsers, getUser, updateUser, deleteUser } = require("../controllers/userController");
 const tokenValidator = require("../middleware/tokenValidator");
@@ -14,5 +17,7 @@ router.get("/:id", tokenValidator, getUser);
 router.put("/:id", tokenValidator, updateUser);
 // Delete user
 router.delete("/:id", tokenValidator, deleteUser);
-
+//Change password route
+router.put("/change-password", verifyToken, changePassword);
+export default router;
 module.exports = router;
