3e889f150553a6e21ba371ea48cafb05ca4b758d
PDiabelko <paigediabelko@gmail.com>
2025-11-11 21:42:42 -0500

    fixed user controllers and user route for pass change

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,23 +1,32 @@
-import express from "express";
-import { changePassword } from "../controllers/userController.js";
-import { verifyToken } from "../middleware/authMiddleware.js";
 const express = require("express");
-const { createUser, getAllUsers, getUser, updateUser, deleteUser } = require("../controllers/userController");
+const {
+  createUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+  changePassword
+} = require("../controllers/userController");
 const tokenValidator = require("../middleware/tokenValidator");
 
 const router = express.Router();
 
 // Create user 
 router.post("/", tokenValidator, createUser);
+
 // Get all users
 router.get("/", tokenValidator, getAllUsers);
-// Get specfic user
+
+// Get specific user
 router.get("/:id", tokenValidator, getUser);
+
 // Update user
 router.put("/:id", tokenValidator, updateUser);
+
 // Delete user
 router.delete("/:id", tokenValidator, deleteUser);
-//Change password route
-router.put("/change-password", verifyToken, changePassword);
-export default router;
+
+// Change password
+router.put("/change-password", tokenValidator, changePassword);
+
 module.exports = router;
