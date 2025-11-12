6a8d0d0699926d27b864b5752d5e4a969b2c3b19
PDiabelko <paigediabelko@gmail.com>
2025-11-11 22:43:02 -0500

    Revert "Reapply "fixed user controllers and user route for pass change""
    This reverts commit 1f684ad51b42e508b70b42854ef6a3e14ec9bd13.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,32 +1,23 @@
+import express from "express";
+import { changePassword } from "../controllers/userController.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
 const express = require("express");
-const {
-  createUser,
-  getAllUsers,
-  getUser,
-  updateUser,
-  deleteUser,
-  changePassword
-} = require("../controllers/userController");
+const { createUser, getAllUsers, getUser, updateUser, deleteUser } = require("../controllers/userController");
 const tokenValidator = require("../middleware/tokenValidator");
 
 const router = express.Router();
 
 // Create user 
 router.post("/", tokenValidator, createUser);
-
 // Get all users
 router.get("/", tokenValidator, getAllUsers);
-
-// Get specific user
+// Get specfic user
 router.get("/:id", tokenValidator, getUser);
-
 // Update user
 router.put("/:id", tokenValidator, updateUser);
-
 // Delete user
 router.delete("/:id", tokenValidator, deleteUser);
-
-// Change password
-router.put("/change-password", tokenValidator, changePassword);
-
+//Change password route
+router.put("/change-password", verifyToken, changePassword);
+export default router;
 module.exports = router;
