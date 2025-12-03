d9c29e21e3e4ac257c9019d119206f5acd45b39b
Collin F <thedrunkninja98@gmail.com>
2025-12-03 15:47:45 -0500

    fix: Changed remaining alert to notif

--- client/portalReact/src/pages/AppointmentViewAdmin.jsx
+++ client/portalReact/src/pages/AppointmentViewAdmin.jsx
@@ -147,7 +147,7 @@ function AppointmentViewAdmin() {
 
     // Basic validation: end time should be after start time
     if (formData.startTime >= formData.endTime) {
-      alert("End time must be after start time");
+      addNotification("End time must be after start time", 'warning');
       return;
     }
 
